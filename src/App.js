22ff99861510517b0f991d55fdebb4961f825e0b
fmaleh@udesa.edu.ar <fmaleh@udesa.edu.ar>
2021-04-06 16:22:43 -0300

    dark out tenemos que hacer con state

--- src/App.js
+++ src/App.js
@@ -4,37 +4,32 @@ import './App.css';
 import Container from './Components/Container/Container';
 import Footer from './Components/Footer/Footer';
 import Search from './Components/Search/Search';
-import styled, { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+
 import{Button} from 'react-bootstrap';
 
-//styked-components.
-const StyledApp = styled.div`
-  color: ${(props) => props.theme.fontColor};
-`;
 
 function App() {
-
-//set de theme 
-  const [theme, setTheme] = useState("light");
-
-  const themeToggler = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  };
- 
   return (
-   // cambio div classname ap por theme provider para el dark mode
-      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      <StyledApp>
-      <Header /> 
-      <Button style={{ marginLeft:"45%"}} variant="danger" onClick={() => themeToggler()}>Change Theme</Button>
+    <div id="wrapper">
+
+<Header /> 
+      <Button style={{ marginLeft:"45%"}} variant="danger" >Change Theme</Button>
        <Search/> 
        <Container/>
        <Footer/>
-      </StyledApp>
-    </ThemeProvider>
+
+	</div>
+	
+    
   );
-}
+
+
+  };
+ 
+ 
+
+
+  
+
 
 export default App;
