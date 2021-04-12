0ea660b42f619aff5cdb7c940db2b2dc1ed55c4c
Agustín <Ihidoype>
2021-04-12 10:51:50 -0300

    Principio change background

--- src/App.js
+++ src/App.js
@@ -3,18 +3,44 @@ import Header from './Components/Header/Header';
 import './App.css';
 import Container from './Components/Container/Container';
 import Footer from './Components/Footer/Footer';
+import{Button} from 'react-bootstrap';
 // import Search from './Components/Search/Search';
 
 
 
-function App() {
-  return (
-    <div id="wrapper">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+        // color: this.props.backgroundColor
+    }
 
-<Header /> 
-       {/* <Search/>  */}
-       <Container/>
-       <Footer/>
+  //   cambiarColor= (nuevoColor)=>{
+  //     console.log("cambiar color a " + nuevoColor);
+  //     // if (this.state.color === this.state.colorOriginal){
+  //     // this.setState({color: nuevoColor})
+  //     // console.log(this.state.color);
+  //     // }
+  //     // else{
+  //     // this.setState({color: this.state.colorOriginal})
+  //     // console.log(this.state.color)
+  //     // }
+  // }
+
+};
+  render(){
+  return (
+    <div id="wrapper" >
+
+      <Header /> 
+      <Button variant="warning" className="buttonChange"
+      //  onClick={this.cambiarColor.bind(this,"black")}
+      >
+        Cambiar fondo
+      </Button>
+      {/* <Search/>  */}
+      <Container/>
+      <Footer/>
 
 	</div>
 	
@@ -24,6 +50,7 @@ function App() {
 
   };
  
+  }
  
 
 
