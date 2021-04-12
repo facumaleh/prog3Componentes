5242aa406865c1b4b8653367e6eea8aa9fcf5fd0
fmaleh@udesa.edu.ar <fmaleh@udesa.edu.ar>
2021-04-11 22:20:00 -0300

    cargar mas tarjetas

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -1,69 +1,85 @@
-import React, {Component, useEffect, useState} from 'react';
 import Character from "../Cards/Card";
 
+import React from "react";
 
-
-export default function Container(props) {
-    
-const [characters, setCharacters] = useState([]);
-  const [searchValue] = useState("");
-
-
-
-  useEffect(() => {
-    getAPI().then((resp) => {
-      setCharacters(resp.results);
-    });
-  }, []);
-
-
-
-
-
-  const getAPI = async () => {
-    const url = "https://randomuser.me/api/?results=21";
-    return await fetch(url).then((result) => result.json());
-  };
-
-    
-
-  const filteredChars = characters.filter((char) => {
-    return char.name.first.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1;
-  });
-
+export default class Container extends React.Component {
+  constructor(props){
+    super(props);
  
-  return (
-    <div className="contenedor">
-        {filteredChars.map((character,idx) => {
-        return (
-          <Character key={idx}
-            firstName={character.name.first}
-            img={character.picture.large}
-            lastName={character.name.last}
-            Email={character.email}
-            city={character.location.city}
-            State={character.location.state}
-            Street={character.location.street.name}
-            StreetNumber={character.location.street.number}
-            Telephone= {character.phone}
-            imgMed={character.picture.medium}
-            Country={character.location.country}
-            Postcode={ character.location.postcode}
-            Bithday= {character.dob.age}
-            Date= {character.dob.date}
-            Registered = {character.registered.date}
-
-          />
-        );
-      })} 
-    </div>
+    this.state = {
+      loading: true,
+      person: null,
+      visible: 6,
+    }; 
+    this.loadmore = this.loadmore.bind(this);
+  }
   
-    
-    
-    
-    
-    )
+  
 
+  async componentDidMount() {
+    const url = "https://randomuser.me/api/?results=21";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ person: data.results, loading: false });
+  }
+  loadmore(){
+    this.setState((old)=>{
+      return{visible: old.visible + 6 }
+    })
+  }
 
+  render() {
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
+
+    if (!this.state.person) {
+      return <div>didn't get a person</div>;
+    }
+
+    return (
+      <div className="contenedor">
+          {this.state.person.slice(0,this.state.visible).map((character,idx) => {
+          return (
+            <Character key={idx}
+              firstName={character.name.first}
+              img={character.picture.large}
+              lastName={character.name.last}
+              Email={character.email}
+              city={character.location.city}
+              State={character.location.state}
+              Street={character.location.street.name}
+              StreetNumber={character.location.street.number}
+              Telephone= {character.phone}
+              imgMed={character.picture.medium}
+              Country={character.location.country}
+              Postcode={ character.location.postcode}
+              Bithday= {character.dob.age}
+              Date= {character.dob.date}
+              Registered = {character.registered.date}
+  
+            />
+          );
+        })} 
+
+          <button type="button" onClick={this.loadmore} >Ver mas tarjetas </button>
+      </div>
+    
+      
+      
+      
+      
+      )
+  
+  }
 }
+
+
+
+
+
+
+
+
+
 
